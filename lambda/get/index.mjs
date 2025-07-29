8e92c92a9152ea907cd7a068c6e89e53bac00c6f
sCuLpToR 699 <mulandimaei76@gmail.com>
2025-07-29 09:27:38 +0000

    api gateway update

--- lambda/get/index.mjs
+++ lambda/get/index.mjs
@@ -1,19 +1,59 @@
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, GetCommand, ScanCommand } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 
 const client = new DynamoDBClient({});
 const docClient = DynamoDBDocumentClient.from(client);
 
-export const coffeeID = async (event) => {
-  const command = new GetCommand({
-    TableName: "praco",
-    Key: {
-      coffeeID: "c1-TEMS",
-    },
-  });
+const tableName = process.env.tableName || "praco";
 
-  const response = await docClient.send(command);
-  console.log(response);
-  return response;
+const createResponse = (statusCode, body) => {
+    const responseBody = JSON.stringify(body);
+    return {
+        statusCode,
+        headers: { "Content-Type": "application/json" },
+        body: responseBody,
+    };
 };
+
+export const getCoffee = async (event) => {
+    const { pathParameters } = event;
+    const { id } = pathParameters || {};
+
+    try {
+        let command;
+        if (id) {
+            command = new GetCommand({
+                TableName: tableName,
+                Key: {
+                    "coffeeId": id,
+                },
+            });
+        }
+        else {
+            command = new ScanCommand({
+                TableName: tableName,
+            });
+        }
+        const response = await docClient.send(command);
+        return createResponse(200, response);
+    }
+    catch (err) {
+        console.error("Error fetching data from DynamoDB:", err);
+        return createResponse(500, { error: err.message });
+    }
+
+}
+
+// export const coffeeID = async (event) => {
+//   const command = new GetCommand({
+//     TableName: "praco",
+//     Key: {
+//       coffeeID: "c1-TEMS",
+//     },
+//   });
+
+//   const response = await docClient.send(command);
+//   console.log(response);
+//   return response;
+// };
 
