1d7b631808dfbcb8e11b5efc9626834a64732d8a
sCuLpToR 699 <mulandimaei76@gmail.com>
2025-08-01 07:54:59 +0000

    react app

--- web-files-front-end/vite.config.js
+++ web-files-front-end/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  console.log('GITPOD_SERVER_URL =', env.GITPOD_SERVER_URL) // Debug log
+
+  return {
+    plugins: [react()],
+    server: {
+      allowedHosts: [env.VITE_GITPOD_SERVER_URL]
+    }
+  }
 })
